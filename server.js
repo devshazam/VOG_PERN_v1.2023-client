73372eca775db15c34f5fb8ae87e2ce397f4d476
Jack Lee <113794114+jacklee137@users.noreply.github.com>
2023-06-12 17:47:36 +0300

    1

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-    origin: ['https://kopi34.ru/', 'https://api.kopi34.ru/']
-  }));
+  origin: 'https://kopi34.ru'
+}));
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', 'https://kopi34.ru/');
-  // res.set('Content-Type', 'text/plain')
-  next();
-});
+// app.use((req, res, next) => {
+//   res.set('Access-Control-Allow-Origin', 'https://kopi34.ru/');
+//   // res.set('Content-Type', 'text/plain')
+//   next();
+// });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
