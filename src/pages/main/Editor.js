67a57d25e1c221ad5fc0ffccb071d743111b4058
shazam-dev <9956221@gmail.com>
2024-05-07 12:08:16 +0300

    api img

--- src/pages/main/Editor.js
+++ src/pages/main/Editor.js
@@ -14,12 +14,6 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 // import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import {fetchEditorObjects} from "../../http/jsonAPI";
 
 const relationArrai = [0.55, 0.4]
@@ -447,7 +441,7 @@ console.log(blockElem.current && blockElem.current.clientWidth)
                             {orders && orders.length > 0 && orders.map((i, index) => {
                                 return(
 
-<img key={index} src={`https://api.kopi34.ru/${i.img}`} style={{height: "40px"}} onClick={() => toJson(`https://api.kopi34.ru/${i.img}`)}/>
+<img key={index} src={`/api/${i.img}`} style={{height: "40px"}} onClick={() => toJson(`/api/${i.img}`)}/>
                                 )
                             })
                             }
